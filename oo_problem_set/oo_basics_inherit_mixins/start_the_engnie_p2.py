949438e66f4c03c40ba616f6fba6459851b3e454
Alexei Bordas <alexei.bordas@gmail.com>
2024-10-07 18:02:31 -0700

    Minor formatting.

--- oo_problem_set/oo_basics_inherit_mixins/start_the_engnie_p2.py
+++ oo_problem_set/oo_basics_inherit_mixins/start_the_engnie_p2.py
@@ -4,7 +4,7 @@ class Vehicle:
 
 class Truck(Vehicle):
     def start_engine(self, speed):
-        return f"Ready to go! Drive {speed}, please!"
+        return super().start_engine + f" Drive {speed}, please!"
 
 # Comments show expected output
 truck1 = Truck()
